3d50393bf399b3b09cb2a16fc112fb1ef548b25b
SanskarShubham <98736288+SanskarShubham@users.noreply.github.com>
2024-02-04 14:08:00 +0530

    learnig request and response

--- node_js/first_app.js
+++ node_js/first_app.js
@@ -4,8 +4,28 @@ const http = require('http');
 // Create a new HTTP server
 const server = http.createServer((req, res) => {
   // Send the response
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Shubham Ojha');
+  // console.log('Shubham Ojha1');
+
+  //  console.log(req.url,req.method, req.headers);
+  // process.exit();
+console.log(req.url);
+  res.setHeader('content-type', 'text/html');
+  res.write('<html>');
+  res.write('<head> <title>Document</title></head>');
+  if (req.url === '/home') {
+    res.write('<body><p>Welcome home</p></body>');
+  }
+  else if (req.url === '/node') {
+    
+    res.write('<body><p> Welcome to my Node Js project</p></body>');
+  }
+  else if (req.url === '/about') {
+    res.write('<body><p> Welcome to About Us page</p></body>');
+  } else {
+    res.write('<body><p>Welcome home</p></body>');
+  }
+  res.write('</html>');
+  res.end();
 });
 
 
