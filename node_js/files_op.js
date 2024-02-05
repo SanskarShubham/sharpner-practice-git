c18adef632b842fdc87d8b0ec1a0e11a4a73a452
SanskarShubham <98736288+SanskarShubham@users.noreply.github.com>
2024-02-05 14:13:39 +0530

    learned how to export func in js

--- node_js/files_op.js
+++ node_js/files_op.js
@@ -1,49 +1,9 @@
-
-const fs = require('fs');
 const http  = require('http');
+const routes  = require('./routes');
 
-const server = http.createServer((req,res)=>{
-    //  console.log(req.method);
-  res.setHeader('content-type', 'text/html');
-
-  if (req.url === '/' && req.method === 'POST') {
-    const body = [];
-    req.on('data',(chunk)=>{
-        body.push(chunk);
-    })
-    return req.on('end',()=>{
-     const parsedData = Buffer.concat(body).toString();
-     const message = parsedData.split('=')[1];   
-     fs.writeFileSync('message.txt',message);
-     res.statusCode = 302;
- 
-     res.setHeader('Location','/');
-     return res.end();
-    })
-   
-  }
-  if (req.url === '/') {
-    res.write('<html>');
-  res.write('<head> <title>file system in JS</title></head>');
-  res.write('<body>');
-  const message = fs.readFileSync('message.txt','utf-8');
-//   console.log(message);
-  if (message.trim() !== '') {
-    res.write(`<p>${message}</p>`);
-  }
-    res.write('<form action="/" method="POST"><input type="text" name="message"><button type="submit">Add</button></form></body>');
-  res.write('</html>');
-  return res.end();
-  
-  }
-
-  
-  res.write('<html>');
-  res.write('<head> <title>file system in JS</title></head>');
-  res.write('<body><p> Welcome to my Node Js project</p></body>');
-  res.write('</html>');
-  res.end();
-});
+// console.log(routes.someText);
+// const server = http.createServer(routes.handler);
+const server = http.createServer(handler);
 
 // Start the server
 server.listen(3000);
---
+++ node_js/routes.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    //  console.log(req.method);
+    res.setHeader('content-type', 'text/html');
+
+    if (req.url === '/' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        return req.on('end', () => {
+            const parsedData = Buffer.concat(body).toString();
+            const message = parsedData.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+
+            res.setHeader('Location', '/');
+            return res.end();
+        })
+
+    }
+    if (req.url === '/') {
+        res.write('<html>');
+        res.write('<head> <title>file system in JS</title></head>');
+        res.write('<body>');
+        const message = fs.readFileSync('message.txt', 'utf-8');
+        //   console.log(message);
+        if (message.trim() !== '') {
+            res.write(`<p>${message}</p>`);
+        }
+        res.write('<form action="/" method="POST"><input type="text" name="message"><button type="submit">Add</button></form></body>');
+        res.write('</html>');
+        return res.end();
+
+    }
+
+
+    res.write('<html>');
+    res.write('<head> <title>file system in JS</title></head>');
+    res.write('<body><p> Welcome to my Node Js project</p></body>');
+    res.write('</html>');
+    res.end();
+}
+module.exports = requestHandler;
+// module.exports.handler = requestHandler;
+// module.exports = {
+//     handler : requestHandler,
+//     someText:'some hard coded text'
+// }
