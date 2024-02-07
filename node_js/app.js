5f86d68f453a4f82515777b1ef0f2951eabeb2ed
SanskarShubham <98736288+SanskarShubham@users.noreply.github.com>
2024-02-07 21:15:47 +0530

    added parser and understad routing concept

--- node_js/app.js
+++ node_js/app.js
@@ -1,13 +1,16 @@
-
 const express  = require('express');
-
+const bodyParser = require("body-parser");
 const app = express();
-
-app.use((req,res,next)=>{
-        console.log('test');
-        next();
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 })
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+        console.log(req.body);
+        res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
         res.send('<h1> welcome to express js</>');
 })
 app.listen(3000);
