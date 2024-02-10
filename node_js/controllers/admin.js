8eee762184822171a2ab981fc80f547fdc3001b2
SanskarShubham <98736288+SanskarShubham@users.noreply.github.com>
2024-02-11 00:26:08 +0530

    added delete functionality

--- node_js/controllers/admin.js
+++ node_js/controllers/admin.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
+    editing:false
   });
 };
 
@@ -15,11 +16,50 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
+exports.getEditProduct = (req, res, next) => {
+    const editing =  req.query.edit;
+    if (!editing) {
+      res.redirect('/')
+    }
+  const productId=  req.params.productId;
+  
+  Product.findById(productId,product =>{
+    res.render('admin/edit-product', {
+      pageTitle: 'edit Product',
+      path: '/admin/edit-product',
+      editing:editing,
+      product:product
+
+    });
+  })
+  
+};
+
+exports.postEditProduct = (req, res, next) => {
+
+  const id = req.body.id;
+  
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+   const product = new Product(id,title, imageUrl, description, price);
+   product.save();
+  res.redirect('/admin/products');
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const id = req.body.id;
+  Product.DeleteById(id,(products)=>{
+    res.redirect('/admin/products');
+  });
+ 
+};
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
--- node_js/models/product.js
+++ node_js/models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,25 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+  
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+    
+        if (this.id) {
+
+          const existingProductIndex = products.findIndex(prod =>prod.id === this.id)
+          const updatedProduct = [...products];
+          updatedProduct[existingProductIndex] = this;
+          fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+            console.log(err);
+          });
+        }else{
+          this.id = Math.random().toString();
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), err => {
+            console.log(err);
+          });
+        }
+      
     });
   }
 
@@ -39,7 +53,18 @@ module.exports = class Product {
     getProductsFromFile(cb);
   }  
   
-  static findById(id,cb) {
+  static DeleteById(id,cb) {
+    getProductsFromFile(products =>{
+      const updatedPrduct = products.filter(p => p.id !== id)
+      fs.writeFile(p, JSON.stringify(updatedPrduct), err => {
+       if (!err) {
+        cb(updatedPrduct)
+       }
+      });
+      
+    });
+  }
+   static findById(id,cb) {
     getProductsFromFile(products =>{
       const product = products.find(p => p.id === id)
       cb(product)
